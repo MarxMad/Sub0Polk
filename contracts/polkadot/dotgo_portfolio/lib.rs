4c354b79403e4b670fd3979163a752d58440bc87
MarxMad <gerardo.gerry.pedrizco@gmail.com>
2025-11-16 09:11:28 -0300

    team members

--- contracts/polkadot/dotgo_portfolio/lib.rs
+++ contracts/polkadot/dotgo_portfolio/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(feature = "std"), no_std, no_main)]
+ls#![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 #[ink::contract(env = ink::env::DefaultEnvironment)]
 mod dotgo_portfolio {
